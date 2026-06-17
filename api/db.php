48c8f682291170ea1c5038f002159be4cf1c4eba
AimarTulgaa <e.enkhtulga@ikhzasag.edu.mn>
2026-06-18 02:02:42 +0800

    Connect to Aiven

--- api/db.php
+++ api/db.php
@@ -1,15 +1,24 @@
 <?php
 // db.php
-$host = 'localhost:3308'; // Таны MySQL-ийн 3308 порт
-$db   = 'riasec_db';
-$user = 'root';
-$pass = ''; 
+$host = 'mysql-37dc3805-ikhzasag-2a09.d.aivencloud.com'; //
+$db   = 'defaultdb'; // Aiven-ийн үндсэн датабаазын нэр хэвээрээ байна
+$user = 'avnadmin';
+$pass = 'AVNS_Y46KyuHT9cudJFWcInl'; 
+$port = '21272'; 
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;port=$port;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+    // Aiven MySQL-д холбогдоход заавал SSL шаардлагатай:
+    PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+];
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass, [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-    ]);
-} catch (PDOException $e) {
-    die("Датабаазтай холбогдож чадсангүй: " . $e->getMessage());
+     $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
+?>
